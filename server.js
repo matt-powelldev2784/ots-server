b716189949d709ff01842d2d0e2b3ccec7afd64e
matt-powelldev2784 <matt.powell.dev2784@gmail.com>
2022-03-22 18:47:35 +0000

    cors middleware

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ connectDB();
 
 //Init Middleware
 app.use(express.json({ extended: false }));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'ots-register-server.herokuapp.com');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 // app.get('/', (req, res) => {
 //     res.send('API Running');
