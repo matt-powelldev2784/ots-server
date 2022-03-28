9084658b7878cf666a6d610497ce1762e1b4d061
matt-powelldev2784 <matt.powell.dev2784@gmail.com>
2022-03-28 19:43:38 +0100

    environment var added

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const config = require('config');
+//const config = require('config');
 
 const User = require('../../models/User');
 
@@ -43,7 +43,7 @@ router.post(
 
             //Return jsonwebtoken
             const payload = { user: { id: user.id } };
-            jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+            jwt.sign(payload, process.env.jwtSecret, { expiresIn: 360000 }, (err, token) => {
                 if (err) {
                     throw err;
                 }
--- server.js
+++ server.js
@@ -23,16 +23,6 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/games', require('./routes/api/games'));
 
-//serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static('client/build'));
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
-
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
