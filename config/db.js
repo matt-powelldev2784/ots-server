1c27b1bd36b10b7edde0df83dfe39a7a07d495e4
Matthew Powell <matt.powell2784@gmail.com>
2022-03-28 19:24:00 +0100

    environment var added

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
+const mongoose = require('mongoose');
+//const config = require('config');
+//const db = config.get('mongoURI');
+require('dotenv').config();
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db);
+    try {
+        await mongoose.connect(process.env.mongoURI);
 
-    console.log("Mongo DB connected");
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+        console.log('Mongo DB connected');
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -11,7 +11,6 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-
 app.use(
     cors({
         credentials: true
