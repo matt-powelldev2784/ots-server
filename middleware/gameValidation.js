aada29adc36a6033336abc69071a2d2fc14eae09
matt-powelldev2784 <matt.powell.dev2784@gmail.com>
2022-05-31 18:51:20 +0100

    api logic updated

---
+++ middleware/gameValidation.js
@@ -0,0 +1,40 @@
+const { check } = require('express-validator');
+const mongoose = require('mongoose');
+
+exports.validateGameAvailability = [
+    check('gameId').not().isEmpty().withMessage('Error. Game not found! Please proivide valid game id.'),
+    check('gameId')
+        .custom((value, { req }) => {
+            return mongoose.Types.ObjectId.isValid(value) === true;
+        })
+        .withMessage('Error. Invalid gameId. The objectID for the game is not found')
+];
+
+exports.validateCreateGame = [
+    check('gameDate', 'Error. Game Date is a required field').not().isEmpty(),
+    check('gameName', 'Error. Game Name is a required field').not().isEmpty()
+];
+
+exports.validateDeleteGame = [
+    check('id')
+        .custom((value, { req }) => {
+            return mongoose.Types.ObjectId.isValid(value) === true;
+        })
+        .withMessage('Error. Invalid gameId. The objectID for the game is not found')
+];
+
+exports.validateSetGameRegister = [
+    check('gameId', 'Error. Game not found! Please proivide valid game id.').not().isEmpty(),
+    check('gameClosed', 'Error. gameClosed is a required field. Please specify true or false').not().isEmpty().isBoolean(),
+    check('gameId', 'Error. Invalid gameId. The objectID for the game is not found').custom((value, { req }) => {
+        return mongoose.Types.ObjectId.isValid(value) === true;
+    })
+];
+
+exports.validateFinalTeam = [
+    check('gameId', 'Error. Game not found! Please proivide valid game id.').not().isEmpty(),
+    check('gameId', 'Error. Invalid gameId. The objectID for the game is not found').custom((value, { req }) => {
+        return mongoose.Types.ObjectId.isValid(value) === true;
+    }),
+    check('finalTeam', 'Error. finalTeam must not be empty and must be an array.').not().isEmpty().isArray()
+];
